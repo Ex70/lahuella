5627d15dbad94cc3d2ed0867b6270398bb712542
Edgar Hernández Martínez <edgaarhdez96@gmail.com>
2021-10-20 19:14:24 -0500

    Proyecto Inicial

---
+++ plantillas/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- page title -->
+    <title>La Huella</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700%7CNunito:400,700,900" rel="stylesheet">
+    <link href="fonts/flaticon/flaticon.css" rel="stylesheet" type="text/css">
+    <link href="fonts/fontawesome/fontawesome-all.min.css" rel="stylesheet" type="text/css">
+    <!-- Fav icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- plugins CSS -->
+    <link href="css/plugins.css" rel="stylesheet">
+    <!-- Colors CSS -->
+    <link href="styles/maincolors.css" rel="stylesheet">
+    <!-- LayerSlider CSS -->
+    <link rel="stylesheet" href="vendor/layerslider/css/layerslider.css">
+    <!-- Switcher Only -->
+    <link rel="stylesheet" id="switcher-css" type="text/css" href="switcher/css/switcher.css" media="all" />
+    <!-- END Switcher Styles -->
+    <link rel="alternate stylesheet" type="text/css" href="styles/maincolors.css" title="maincolors" media="all" />
+    <link rel="alternate stylesheet" type="text/css" href="styles/kindergarten.css" title="kindergarten" media="all" />
+    <link rel="alternate stylesheet" type="text/css" href="styles/playtime.css" title="playtime" media="all" />
+    <link rel="alternate stylesheet" type="text/css" href="styles/preschool.css" title="preschool" media="all" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-76370337-3"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-76370337-3');
+    </script> -->
